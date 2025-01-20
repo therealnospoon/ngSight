a4bb1a20e98bf466e51d870adc07be4d9798cbb0
Tim Kim <timkimdesigns@gmail.com>
2025-01-20 10:42:39 -0800

    Created orders table with mock data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './section-orders.component.html',
   styleUrl: './section-orders.component.scss'
 })
-export class SectionOrdersComponent {
+export class SectionOrdersComponent implements OnInit {
+  constructor() {} 
 
+  orders: Order[] = [
+    { 
+      id: 1, customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, 
+      total: 230, placed: new Date(2020, 12, 1), fulfilled: new Date(2020, 12, 2)
+    },
+    { 
+      id: 2, customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, 
+      total: 230, placed: new Date(2020, 12, 1), fulfilled: new Date(2020, 12, 2)
+    },
+    { 
+      id: 3, customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, 
+      total: 230, placed: new Date(2020, 12, 1), fulfilled: new Date(2020, 12, 2)
+    },
+    { 
+      id: 4, customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, 
+      total: 230, placed: new Date(2020, 12, 1), fulfilled: new Date(2020, 12, 2)
+    },
+    { 
+      id: 5, customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, 
+      total: 230, placed: new Date(2020, 12, 1), fulfilled: new Date(2020, 12, 2)
+    },
+  ];
+
+  ngOnInit(): void {
+    
+  }
 }
